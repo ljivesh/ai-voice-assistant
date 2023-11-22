821972adcd365fcc50c6fdf20848be0882e0a57e
Menma <menma@pop-os.localdomain>
2023-11-22 22:45:46 +0530

    typo in BASEURL fixed

--- src/modules/envirnoment.js
+++ src/modules/envirnoment.js
@@ -1,2 +1,2 @@
-export const BASEURL = "ec2-13-233-207-255.ap-south-1.compute.amazonaws.com/api/";
+export const BASEURL = "ec2-13-233-207-255.ap-south-1.compute.amazonaws.com";
 console.log("Base URl", BASEURL);
